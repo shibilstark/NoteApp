6de3d1c808008e6419737086aa6e32cb1d6e2901
shibilstark <shibilhassankmangode@gmail.com>
2022-08-24 19:53:11 +0530

    completed functionality

--- note_app/lib/presentation/screens/home.dart
+++ note_app/lib/presentation/screens/home.dart
@@ -49,15 +49,38 @@ class HomeScreenBody extends StatelessWidget {
     return Container(
       child: BlocBuilder<NoteCubit, NoteState>(
         builder: (context, state) {
-          return ListView.separated(
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text(state.notes[index].title),
-                  subtitle: Text(state.notes[index].discription),
-                );
-              },
-              separatorBuilder: (context, index) => SizedBox(height: 10),
-              itemCount: state.notes.length);
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ListView.separated(
+                itemBuilder: (context, index) {
+                  return Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: primaryColor.withOpacity(0.1)),
+                    padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            state.notes[index].title,
+                            style: TextStyle(
+                                fontSize: 19,
+                                fontWeight: FontWeight.w600,
+                                color: primaryColor),
+                          ),
+                          SizedBox(height: 5),
+                          Text(state.notes[index].discription,
+                              style: TextStyle(
+                                fontSize: 15,
+                              )),
+                        ]),
+                    // title: Text(state.notes[index].title),
+                    // subtitle: Text(state.notes[index].discription),
+                  );
+                },
+                separatorBuilder: (context, index) => SizedBox(height: 10),
+                itemCount: state.notes.length),
+          );
         },
       ),
     );
